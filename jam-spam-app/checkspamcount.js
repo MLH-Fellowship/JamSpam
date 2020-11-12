0d0619f5f9bc9e04e62a167b537619fed9cb96ae
Ajwad Shaikh <shaikhajwad10@gmail.com>
2020-11-13 00:33:39 +0530

    Import New Model to Bot

--- jam-spam-app/checkspamcount.js
+++ jam-spam-app/checkspamcount.js
@@ -1,42 +1,51 @@
 /* Spam blob */
 const spam = [
-  "readme",
-  "relevant issues",
-  "relevant docstrings",
   "versionchanged directives",
+  "save time",
+  "existing algorithm",
+  "existing functionality",
+  "naming conventions",
+  "understand areas",
   "changelog entry",
   "run automatically",
-  "save time",
-  "beginners create",
-  "basic blogging",
-  "main interface",
-  "release noteshttpsgithub",
-  "comevanwesbuildreleases changeloghttpsgithub",
-  "fixes nnn",
-  "additional information",
-  "stepbystep reproduction",
-  "create readmekorean",
-  "event handling",
-  "pr title",
-  "contribution guide",
+  "style guidelines",
+  "md file",
+  "filename follow",
+  "auto close",
+  "update readme",
+  "contributing document",
+  "update contributing",
+  "relevant docstrings",
+  "relevant motivation",
+  "relevant issues",
+  "pr fixes",
+  "breaking change",
+  "change requires",
+  "delete options",
+  "added tests",
+  "update change",
+  "quickly create",
+  "provide instructions",
+  "md describe",
+  "issue exists",
+  "change add",
+  "feature works",
+  "documentation update",
+  "relevant details",
+  "test configuration",
 ];
 
 /* Check count of a substring in the string */
 const counter = (main_str, sub_str) => {
   main_str += "";
   sub_str += "";
-
-  if (sub_str.length <= 0) {
-    return main_str.length + 1;
-  }
-
-  subStr = sub_str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-  return (main_str.match(new RegExp(subStr, "gi")) || []).length;
+  return (main_str.match(new RegExp(sub_str, "gi")) || []).length;
 };
 
 /* Count the total spam keywords in the PR */
 const spam_count = (text_corpus) => {
   let count = 0;
+  text_corpus = text_corpus.replace(/[^a-zA-Z0-9 \n\.]/g, ' ');
   for (word of spam) {
     var countSpam = counter(text_corpus, word);
     count = count + countSpam;
